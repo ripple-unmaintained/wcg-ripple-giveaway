b7ea167e190344ba888e8254a7e9e9c0afbe210d
Steven Zeiler <zeiler.steven@gmail.com>
2013-11-20 14:52:08 -0800

    [BUG] Use strong_params instead of attr_accessible.

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,10 +1,8 @@
 class ClaimsController < ApplicationController
   def update
-  	claim = Claim.find(params[:claim_id])
-  	claim.update_attributes({
-  	  transaction_status: params[:transaction_status],
-  	  transaction_hash: params[:transaction_hash]
-  	})
+  	claim_params = params.require(:transaction_status, :transaction_hash, :claim_id)
+  	claim = Claim.find(claim_params[:claim_id])
+  	claim.update_attributes(claim_params)
   	render json: claim
   end
 end
--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,12 +1,11 @@
 class Claim < ActiveRecord::Base
 	validates_presence_of :member_id, :rate, :points
-  attr_accessible :transaction_hash, :transaction_status
   belongs_to :user, foreign_key: 'member_id'
 
   def enqueue
     Queue.push({
       unique_id: self.id,
-      ripple_address: self.user.ripple_address
+      ripple_address: self.user.ripple_address,
       xrp_amount: self.points / self.rate
     })
   end
