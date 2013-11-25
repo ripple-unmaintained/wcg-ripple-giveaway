b011466fe6959ac678ad42cef2513e209958547c
Steven Zeiler <zeiler.steven@gmail.com>
2013-11-25 11:45:28 -0800

    [FEATURE] Use ENV variable for redis config url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 
-uri = URI.parse('redis://redistogo:bfa171d80c28c437e382a2cf155b2c62@grideye.redistogo.com:9390/')
+uri = URI.parse(ENV['REDISTOGO_URL'])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 if REDIS.get('xrp_to_give_away').empty?
