695df10d9babe571d959874703b77d6d01aa2759
Steven Zeiler <zeiler.steven@gmail.com>
2013-11-25 16:32:00 -0800

    [BUG] check that REDIS key is nil? not .empty?

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
-require 'open-uri'
-
 uri = URI.parse(ENV['REDISTOGO_URL'])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-if REDIS.get('xrp_to_give_away').empty?
+if REDIS.get('xrp_to_give_away').nil?
   REDIS.set('xrp_to_give_away', 300000)
 end
