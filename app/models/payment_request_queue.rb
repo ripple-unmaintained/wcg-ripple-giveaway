dcd530a99dbabd655b24052426293a3055d8afa0
Steven Zeiler <zeiler.steven@gmail.com>
2013-11-26 12:57:06 -0800

    [FEATURE] Add queueing functionality for creating nonexistant SQS queues.

--- app/models/payment_request_queue.rb
+++ app/models/payment_request_queue.rb
@@ -1,11 +1,24 @@
 class PaymentRequestQueue
+  QUEUE_NAME = ENV['PAYMENT_REQUESTS_QUEUE_NAME']
   class << self
-    def queue
-    	@queue ||= AWS::SQS::Queue.new(ENV['PAYMENT_REQUESTS_QUEUE_NAME'])
-    end
 
     def push(hash)
-	    @queue ||= queue.send_message(hash.to_json)
-	  end
+      queue.send_message(hash.to_json)
+    end
+
+  protected
+
+    def sqs
+    	@sqs ||= AWS::SQS.new
+    end
+
+    def queue
+      @queue ||= begin
+        queue = sqs.queues.named(QUEUE_NAME)
+      rescue => e
+        queue = sqs.queues.create(QUEUE_NAME)
+      end
+    end
+
   end
 end
