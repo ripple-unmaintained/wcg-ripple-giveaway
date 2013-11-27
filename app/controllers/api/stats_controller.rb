a52a87f598676ab8d5c4212aac2ad62f0eb21e0a
Justin Lynn <j@jaesharp.com>
2013-11-26 19:42:19 -0800

    handle case where user has no statistics

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -4,8 +4,16 @@ class Api::StatsController < ApplicationController
       wcg_user_response = Wcg.get_team_member(session[:user][:username])
       user = User.where(member_id: session[:user][:member_id]).first
 
-      total_run_time = wcg_user_response['stats'][:RunTime].to_i
-      total_points = wcg_user_response['stats'][:Points].to_i
+      total_run_time = 0
+      total_points = 0
+
+      if wcg_user_response['stats']
+        total_run_time = wcg_user_response['stats'][:RunTime].to_i
+        total_points = wcg_user_response['stats'][:Points].to_i
+      else
+        wcg_user_response['stats'] = Hash.new
+      end
+
       wcg_user_response['stats'][:RunTime] = total_run_time - user.initial_run_time.to_i
       wcg_user_response['stats'][:Points] = total_points - user.initial_points.to_i
 
