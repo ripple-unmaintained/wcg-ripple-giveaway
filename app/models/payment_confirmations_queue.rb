684baaead790fb8f43ba4e313e1ebe03f0624959
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-18 11:34:06 -0800

    [CHORE] Handle decontructed uuids from the confirmations queue.

--- app/models/payment_confirmations_queue.rb
+++ app/models/payment_confirmations_queue.rb
@@ -4,12 +4,29 @@ class PaymentConfirmationsQueue
   def self.process_confirmed_payments
     self.queue.poll do |message|
       confirmation = JSON.parse(message.body)
-      claim = Claim.find(confirmation['unique_id'])
+      claim = Claim.find(parse_uid(confirmation['unique_id']))
       claim.confirm_payment(confirmation)
     end
   end
 
   protected
+
+  def parse_uid(uid)
+    if uid.match('-')
+      uid
+    else
+      # postgres uuid formatted in blocks of:
+      # 8,4,4,4,12
+      blocks = [
+        uid[0..7],
+        uid[8..11],
+        uid[12..15],
+        uid[16..19],
+        uid[20..31]
+      ]
+      blocks.join('-')
+    end
+  end
 
   def self.sqs
     @sqs ||= AWS::SQS.new
