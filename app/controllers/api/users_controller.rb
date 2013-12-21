aa50db00bfa6501eca8c35f289bbade16a4fb9d5
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-20 16:43:50 -0800

    [FEATURE] Add flash notice after user registers with information about payouts

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,12 @@ class Api::UsersController < ApplicationController
       verification_code: params.require(:verification_code)
   	})
     if user.errors.messages.empty?
+			if session[:language] && session[:language] == 'chinese'
+				flash[:notice] = "欢迎注册 ComputingforGood.org. 我们每天都会计算一次你为 WCG 贡献的小时数，并发出 XRP，请在24小时内检查。记得参与计算哦。"
+			else
+				flash[:notice] = "Congratulations, you're now registered with ComputingforGood.org. We calculate your hours contributed and then pay out XRP daily, so please check back in 24 hours. In the meantime, compute away!"
+		  end
+
       session[:user] = { member_id: user.member_id, username: user.username }
       render json: { user: user.attributes }
     else
