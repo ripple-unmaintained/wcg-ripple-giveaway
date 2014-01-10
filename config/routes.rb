5c6b102672b257254fedc2f3f6244d84a4e71706
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-10 12:31:20 -0800

    [FEATURE] Remove api routes, add new web stats routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 WcgGiveaway::Application.routes.draw do
   namespace :api do
-  	resources :users, only: :create
-  	resources :stats, only: :index
-    get 'stats/global', to: 'stats#global'
-    resources :sessions, only: :create
     post 'session/language', to: 'sessions#language'
     get 'session', to: 'sessions#show'
   end
@@ -14,11 +10,11 @@ WcgGiveaway::Application.routes.draw do
 		get 'users/by-username/:username', to: 'users#show'
   end
 
+  get 'stats/username/:username', to: 'stats#username'
+  get 'stats/member/:member_id', to: 'stats#show'
+  get 'stats', to: 'stats#index'
+    
   get 'register', to: 'users#new'
-  get 'my-stats', to: 'stats#show'
-
-  get 'login', to: 'sessions#new'
-  get 'logout', to: 'sessions#destroy'
 
   root to: 'application#index'
 end
