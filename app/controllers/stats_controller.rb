e302f93190d7edd16306bae7a442dd016d2ba522
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-10 12:31:00 -0800

    [FEATURE] Set up stats web controller by member id to server user data.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,19 @@
 class StatsController < ApplicationController
+  def username
+    member_id = User.find_by_username(params[:username]).try(:member_id)
+    if member_id
+      flash[:notice] = "No users with username #{params[:username]} is registered"
+      redirect_to '/'
+    else
+      redirect_to "/stats/member/#{member_id}"
+    end
+  end
+
   def show
-  	if !session[:user]
-  	  redirect_to '/login'
-  	end
+    @member = User.find_by_member_id(params.require[:member_id])
+    unless !@member
+      flash[:notice] = "No member has registered with id #{params[:member_id]}"
+  	  redirect_to '/'
+    end
   end
 end
